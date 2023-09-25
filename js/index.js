846f101d459fde955a537fb9ec02bdcc10a585b5
Nathan tshimbela <87193016+NCB-DEV@users.noreply.github.com>
2023-09-25 03:56:48 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let button=document.getElementById('button');
+let nav=document.getElementById('nav');
+button.addEventListener('click',afficher);
+function afficher(){
+    nav.style.display="block";
+}
